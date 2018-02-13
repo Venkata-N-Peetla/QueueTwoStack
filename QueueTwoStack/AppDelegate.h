02185a13556e96f5a076d72592c7f28e615b9d8a
Venkata Narasimham Peetla <venkata.n.peetla@kp.org>
2018-02-12 20:38:40 -0800

    Initial Commit

---
+++ QueueTwoStack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QueueTwoStack
+//
+//  Created by Venkata Narasimham Peetla on 2/12/18.
+//  Copyright © 2018 PEETLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QueueTwoStack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QueueTwoStack
+//
+//  Created by Venkata Narasimham Peetla on 2/12/18.
+//  Copyright © 2018 PEETLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
